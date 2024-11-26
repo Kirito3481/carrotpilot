51bddae7d9d7c874bd0438d60fa844471ce588cb
Kirito3481 <47619491+Kirito3481@users.noreply.github.com>
2024-11-26 20:32:58 +0900

    Fixed typo in hyundai/values.py

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -337,7 +337,7 @@ class CAR(Platforms):
       HyundaiCarDocs("Hyundai Ioniq 5 N (with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_m])),
     ],
     CarSpecs(mass=2200, wheelbase=3.00, steerRatio=12.54),
-    falgs=HyundaiFlags.EV,
+    flags=HyundaiFlags.EV,
   )
   HYUNDAI_IONIQ_6 = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Hyundai Ioniq 6 (with HDA II) 2023-24", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))],
